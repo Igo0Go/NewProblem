dedbb2ad7ebaf2578b999c1fe320162c0f3d12ef
CorpseNoble <bespe4uk98@yandex.ru>
2021-11-06 21:51:16 +0700

    Doors&Rooms

---
+++ Assets/Scripts/GameSystems/Door.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : TwoStateInteractiveObject
+{
+    [Header("Only two rooms")]
+    public List<Room> Rooms = new List<Room>();
+    public bool Open
+    {
+        get
+        {
+            return open;
+        }
+        set
+        {
+            open = value;
+        }
+    }
+
+    public bool Energy
+    {
+        get
+        {
+            return energy;
+        }
+        set
+        {
+            if (value)
+                Open = false;
+
+            energy = value;
+        }
+    }
+
+    [SerializeField] private bool open = false;
+    [SerializeField] private bool energy = true;
+    public Room GetOtherRoom(Room room)
+    {
+        if (Rooms[0] == room)
+            return Rooms[1];
+        else
+            return Rooms[0];
+    }
+
+    protected override void Command()
+    {
+        base.Command();
+
+        if (Energy)
+            Open = !Open;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameSystems/Room.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public List<Door> Doors = new List<Door>();
+
+    public bool InfiniteSpace
+    {
+        get
+        {
+            return infiniteSpace;
+        }
+        set
+        {
+            if (value)
+                foreach (var d in Doors)
+                    d.Open = true;
+            else
+            {
+                AutoOxygen = value;
+            }
+
+
+            InfiniteSpace = value;
+        }
+    }
+
+    public bool Energy
+    {
+        get
+        {
+            return energy;
+        }
+        set
+        {
+            if (!InfiniteSpace)
+                AutoOxygen = value;
+
+            energy = value;
+        }
+    }
+
+    [SerializeField] public bool AutoOxygen = true;
+
+    [SerializeField] private bool energy = true;
+    [SerializeField] private bool infiniteSpace = false;
+
+    [Range(1, 10), SerializeField] public float volume = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
