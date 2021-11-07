413a7ce9853550d2571d37e3206bb7f877d8d27d
Игорь Чудакин <Chudakin98@gmail.com>
2021-11-07 19:30:29 +0700

    система здоровья + аптечки

--- Assets/Scripts/Character/HealthPlayer.cs
+++ Assets/Scripts/Character/HealthPlayer.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class HealthPlayer : MonoBehaviour
 {
+    [SerializeField] private int damagePerSeconds = 5;
     [SerializeField] private float maxHealth = 100;
 
     [SerializeField] private float curHealth = 100;
@@ -16,43 +18,72 @@ public class HealthPlayer : MonoBehaviour
         {
             curHealth = value;
 
+            if(curHealth > maxHealth)
+            {
+                curHealth = maxHealth;
+            }
+
             if (value <= 0)
                 Death();
         }
     }
     public float MaxHealth { get => maxHealth; private set => maxHealth = value; }
 
-    public static event Action Damaged;
-    public static event Action Healed;
+    public static event Action<float> Damaged;
+    public static event Action<float> Healed;
     public static event Action Dead;
 
-    // Use this for initialization
-    void Start()
-    {
+    private Collider bufer;
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void GetDamage(float damage)
     {
         CurHealth -= damage;
-        Damaged?.Invoke();
+        Damaged?.Invoke(CurHealth);
     }
 
     public void GetHeal(float heal)
     {
         CurHealth += heal;
-        Healed?.Invoke();
+        Healed?.Invoke(CurHealth);
     }
 
     public void Death()
     {
         Dead?.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("DamageZone"))
+        {
+            if(bufer == null)
+            {
+                bufer = other;
+                StartCoroutine(DamagePerUnit());
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+{
+    if (other.CompareTag("DamageZone"))
+    {
+        if (other == bufer)
+        {
+            StopCoroutine(DamagePerUnit());
+            bufer = null;
+        }
+    }
+}
+
+    private IEnumerator DamagePerUnit()
+    {
+        while(bufer != null)
+        {
+            GetDamage(damagePerSeconds);
+            yield return new WaitForSeconds(1);
+        }
     }
 
 }
--- Assets/Scripts/Character/UIController.cs
+++ Assets/Scripts/Character/UIController.cs
@@ -6,10 +6,17 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     [SerializeField] private Text messageText;
+    [SerializeField] private Slider healthSlider;
+
+    private float targetHealthValue;
 
     void Awake()
     {
         InteractionController interactionController = FindObjectOfType<InteractionController>();
+        HealthPlayer.Damaged += OnGetDamage;
+        HealthPlayer.Healed += OnGetDamage;
+        HealthPlayer.Dead += OnDead;
+
         interactionController.changeMessageEvent += ChangeMessage;
 
         ChangeMessage(string.Empty);
@@ -18,5 +25,38 @@ public class UIController : MonoBehaviour
     private void ChangeMessage(string newMessage)
     {
         messageText.text = newMessage;
+    }
+
+    private void OnGetDamage(float value)
+    {
+        MoveSlider(value);
+    }
+
+    private void OnGetHeal(int value)
+    {
+        MoveSlider(value);
+    }
+
+    private void OnDead()
+    {
+
+    }
+
+    private void MoveSlider(float value)
+    {
+        targetHealthValue = value;
+        StopCoroutine(ChangeHealth());
+        StartCoroutine(ChangeHealth());
+    }
+
+    private IEnumerator ChangeHealth()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            healthSlider.value = Mathf.Lerp(healthSlider.value, targetHealthValue, t);
+            yield return null;
+        }
     }
 }
