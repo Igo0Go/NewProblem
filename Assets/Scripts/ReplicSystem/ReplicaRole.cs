1726a12d078ed16f3f5337728260b6091fee8a48
menem8888 <menem8888@gmail.com>
2021-11-07 20:05:19 +0700

    Roles

--- Assets/Scripts/ReplicSystem/ReplicaRole.cs
+++ Assets/Scripts/ReplicSystem/ReplicaRole.cs
@@ -3,6 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ReplicaRole", menuName = "ReplicaRole")]
 public class ReplicaRole : ScriptableObject
 {
-    public Sprite roleIcon;
     public Color roleTextColor;
 }
